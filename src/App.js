9f7a5dd2879aba71fedd1853754d70b7543e22be
sanjivkrish <sanjiv436.k@gmail.com>
2019-07-26 00:21:22 +0200

    call markdown renderer in action

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import Header from './components/Header/Header'
 import Dropdown from './components/Dropdown/Dropdown'
+import MarkdownConvertor from './components/MarkdownConvertor'
+
+// Sample templates
+let templateList = [{
+  id: "template1",
+  content : `# This is a header\n\nAnd this is a paragraph`
+}, {
+  id: "template2",
+  content : `# Sample template
+
+  Following is a list component.
+  
+  * First Point
+  * Second Point`
+}];
 
 function App() {
-  const [template, setTemplate] = React.useState();
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const [template, setTemplate] = React.useState(templateList[activeIndex]);
 
-  const templateList = [{
-    "id": "template1",
-    "content" : ""
-  }, {
-    "id": "template2",
-    "content" : ""
-  }];
-
+  // On template toggle
   const onChange = (value) => {
-    setTemplate(() => templateList[value])
+    setActiveIndex(() => value);
+    setTemplate(() => { return { ...templateList[value] }})
+  }
+
+  // On content change
+  const handleMarkdownChange = event => {
+    let newContent = event.target.value
+
+    templateList[activeIndex].content = newContent
+ 
+    setTemplate(() => { return { ...templateList[activeIndex] }})
+
   }
   
   return (
     <div className="App">
       <Header title="CONTENT EDITOR" />
       <Dropdown label="Select a template" menu={templateList} onChange={onChange} />
+      <MarkdownConvertor value={template.content} onChange={handleMarkdownChange} />
     </div>
   );
 }
