4e30b1a6068f2fde63672ccc51ba7fe596cefa78
sanjivkrish <sanjiv436.k@gmail.com>
2019-07-25 18:36:05 +0200

    create material themed header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+
+import './styles.css';
+
+const Header = props => {
+    return (
+        <AppBar position="static" color="primary">
+            <Toolbar className="header-title">
+                <Typography varient="h6" color="inherit">
+                    { props.title }
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header
